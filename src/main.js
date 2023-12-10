361a279586b5526f4499b9b58f9cb225da689cd5
annalinkova <linkovanne@yandex.ru>
2023-12-10 16:26:58 +0100

    module1-task3: added components

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import { render } from './render';
+import NewEventButtonView from './view/new-event-button-view';
+import FilterFormView from './view/filter-form-view';
+import TripPresenter from './presenter/trip-presenter';
+
+const siteHeaderElement = document.querySelector('.trip-main');
+const tripControlsFilters = document.querySelector('.trip-controls__filters');
+const siteTripEventsElement = document.querySelector('.trip-events');
+
+const tripPresenter = new TripPresenter({
+  tripContainer: siteTripEventsElement
+});
+
+render(new NewEventButtonView(), siteHeaderElement);
+render(new FilterFormView(), tripControlsFilters);
+tripPresenter.init();
---
+++ src/presenter/trip-presenter.js
@@ -0,0 +1,24 @@
+import { render } from '../render';
+import EventItemView from '../view/event-item-view';
+import EventListView from '../view/event-list-view';
+import SortFormView from '../view/sort-form-view';
+import EditEventFormView from '../view/edit-event-form-view';
+
+export default class TripPresenter {
+  eventListComponent = new EventListView();
+
+  constructor({ tripContainer }) {
+    this.tripContainer = tripContainer;
+  }
+
+  init() {
+    render(new SortFormView(), this.tripContainer);
+    render(this.eventListComponent, this.tripContainer);
+    render(new EditEventFormView(), this.eventListComponent.getElement());
+
+    const tripsCounter = 3;
+    for (let i = 0; i < tripsCounter; i++) {
+      render(new EventItemView(), this.eventListComponent.getElement());
+    }
+  }
+}
