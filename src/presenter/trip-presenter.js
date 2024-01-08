0770a4d3929edca924ef7edf03d3675d185833c9
annalinkova <linkovanne@yandex.ru>
2024-01-09 00:38:44 +0100

    module5-task2: added sort handling

--- src/presenter/trip-presenter.js
+++ src/presenter/trip-presenter.js
@@ -1,4 +1,4 @@
-import {render} from '../framework/render';
+import {remove, render} from '../framework/render';
 import EventListView from '../view/event-list-view';
 import EmptyEventListView from '../view/empty-event-list-view';
 import SortView from '../view/sort-view';
@@ -7,7 +7,9 @@ import FiltersView from '../view/filters-view';
 // import AddEventFormView from '../view/add-event-form-view';
 import {generateFilter} from '../mock/filter';
 import EventPresenter from './event-presenter';
+import {sortType} from '../const';
 import {updateEvent} from '../utils/common';
+import {sortByDay, sortByPrice, sortByTime} from '../utils/events';
 
 export default class TripPresenter {
   /**
@@ -36,9 +38,10 @@ export default class TripPresenter {
   };
 
   #newEventButton = new NewEventButtonView({ onClick: this.#handleAddEventForm });
-  #sortComponent = new SortView();
+  #sortComponent = null;
   #eventListComponent = new EventListView();
   #emptyEventListComponent = new EmptyEventListView();
+
 
   /**
    * @type {boolean}
@@ -56,6 +59,10 @@ export default class TripPresenter {
    * @type {Array}
    */
   #destinations = [];
+  /**
+   * @type {SortType}
+   */
+  #currentSortType = sortType.DAY;
 
   constructor({headerContainer, tripFiltersContainer, tripContainer, tripModel}) {
     this.#headerContainer = headerContainer;
@@ -97,16 +104,69 @@ export default class TripPresenter {
   /**
    * @method
    */
-  // #clearEventList() {
-  //   this.#eventPresenter.forEach((presenter) => presenter.destroy());
-  //   this.#eventPresenter.clear();
-  // }
+  #clearEventList() {
+    remove(this.#sortComponent);
+    this.#eventPresenter.forEach((presenter) => presenter.destroy());
+    this.#eventPresenter.clear();
+  }
+
+  #renderEventList() {
+    this.#renderSort();
+    render(this.#eventListComponent, this.#tripContainer);
+
+    for (const event of this.#events) {
+      this.#renderEvent(event, this.#offers, this.#destinations);
+    }
+  }
 
   // #renderAddEventForm() {
   //   const addEventForm = new AddEventFormView();
   //
   //   render(addEventForm, this.#eventListComponent.element);
   // }
+
+  /**
+   * @method
+   * @param {SortType} type
+   */
+  #sortEvents(type) {
+    switch (type) {
+      case sortType.DAY:
+        this.#events.sort(sortByDay);
+        break;
+      case sortType.TIME:
+        this.#events.sort(sortByTime);
+        break;
+      case sortType.PRICE:
+        this.#events.sort(sortByPrice);
+        break;
+    }
+
+    this.#currentSortType = type;
+  }
+
+  /**
+   * @method
+   * @param {SortType} type
+   */
+  #handleSortTypeChange = (type) => {
+    if (this.#currentSortType === type) {
+      return;
+    }
+
+    this.#sortEvents(type);
+    this.#clearEventList();
+    this.#renderEventList();
+  };
+
+  #renderSort() {
+    this.#sortComponent = new SortView({
+      currentSortType: this.#currentSortType,
+      onSortTypeChange: this.#handleSortTypeChange
+    });
+
+    render(this.#sortComponent, this.#tripContainer);
+  }
 
   #renderFilters(events) {
     /**
@@ -133,11 +193,6 @@ export default class TripPresenter {
       return render(this.#emptyEventListComponent, this.#tripContainer);
     }
 
-    render(this.#sortComponent, this.#tripContainer);
-    render(this.#eventListComponent, this.#tripContainer);
-
-    for (const event of this.#events) {
-      this.#renderEvent(event, this.#offers, this.#destinations);
-    }
+    this.#renderEventList();
   }
 }
--- src/view/sort-view.js
+++ src/view/sort-view.js
@@ -1,38 +1,66 @@
 import AbstractView from '../framework/view/abstract-view';
+import {sortType} from '../const';
+import {sort} from '../utils/sort';
 
-function createSortTemplate() {
+/**
+ * method to create item of sort form
+ * @method
+ * @param {SortType} currentSortType
+ * @param {SortTypeItemObject} sortItem
+ */
+function createSortItemTemplate(currentSortType, sortItem) {
+  const {name, disabled} = sortItem;
+
   return (
-    `<form class="trip-events__trip-sort  trip-sort" action="#" method="get">
-      <div class="trip-sort__item  trip-sort__item--day">
-        <input id="sort-day" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-day" checked>
-        <label class="trip-sort__btn" for="sort-day">Day</label>
-      </div>
+    `<div class="trip-sort__item  trip-sort__item--${name}">
+      <input id="sort-${name}" class="trip-sort__input  visually-hidden"
+        type="radio" name="trip-sort" value="sort-${name}" data-sort-type="${name}"
+        ${currentSortType === name ? 'checked' : ''} ${disabled ? 'disabled' : ''}>
+      <label class="trip-sort__btn" for="sort-${name}">${name}</label>
+    </div>`
+  );
+}
 
-      <div class="trip-sort__item  trip-sort__item--event">
-        <input id="sort-event" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-event" disabled>
-        <label class="trip-sort__btn" for="sort-event">Event</label>
-      </div>
-
-      <div class="trip-sort__item  trip-sort__item--time">
-        <input id="sort-time" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-time">
-        <label class="trip-sort__btn" for="sort-time">Time</label>
-      </div>
-
-      <div class="trip-sort__item  trip-sort__item--price">
-        <input id="sort-price" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-price">
-        <label class="trip-sort__btn" for="sort-price">Price</label>
-      </div>
-
-      <div class="trip-sort__item  trip-sort__item--offer">
-        <input id="sort-offer" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-offer" disabled>
-        <label class="trip-sort__btn" for="sort-offer">Offers</label>
-      </div>
+function createSortTemplate(currentSortType) {
+  return (
+    `<form class="trip-events__trip-sort trip-sort" action="#" method="get">
+      ${Object.values(sort).map((value) => createSortItemTemplate(currentSortType, value)).join('')}
     </form>`
   );
 }
 
 export default class SortView extends AbstractView {
-  get template() {
-    return createSortTemplate();
+  /**
+   * @type {SortType}
+   */
+  #currentSortType = sortType.DAY;
+  /**
+   * @type {(null|function)}
+   */
+  #handleSortTypeChange = null;
+
+  constructor({ currentSortType, onSortTypeChange }) {
+    super();
+
+    this.#currentSortType = currentSortType;
+
+    this.#handleSortTypeChange = onSortTypeChange;
+    this.element.addEventListener('click', this.#sortTypeChangeHandler);
   }
+
+  get template() {
+    return createSortTemplate(this.#currentSortType);
+  }
+
+  /**
+   * @method
+   * @param {Event} event
+   */
+  #sortTypeChangeHandler = (event) => {
+    if (event.target?.control?.tagName !== 'INPUT' || event.target?.control.disabled) {
+      return;
+    }
+    event.preventDefault();
+    this.#handleSortTypeChange(event.target?.control?.dataset?.sortType);
+  };
 }
